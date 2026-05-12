d4f677a6dfe4f4077470e91e4412131af754178c
gabsswz <bielg123br@gmail.com>
2026-05-12 20:06:27 -0300

    classe Motorista

---
+++ Classes/Motorista.php
@@ -0,0 +1,49 @@
+<?php
+
+    class Motorista {
+        private string $nome;
+        private string $cpf;
+        private string $cnh;
+        private string $validadeCnh;
+
+        public function __construct(string $nome, string $cpf, string $cnh, string $validadeCnh) {
+            $this->nome = $nome;
+            $this->cpf = $cpf;
+            $this->cnh = $cnh;
+            $this->validadeCnh = $validadeCnh;
+        } 
+
+        public function setNome(string $novoNome) : void {
+            $this->nome = $novoNome;
+        }
+
+        public function getNome() : string {
+            return $this->nome;
+        }
+
+        public function setCpf(string $novoCpf) : void {
+            $this->cpf = $novoCpf;
+        }
+
+        public function getCpf() : string {
+            return $this->cpf;
+        }
+
+        public function setCnh($novaCnh) : void {
+            $this->cnh = $novaCnh;
+        }
+
+        public function getCnh() : string {
+            return $this->cnh;
+        }
+
+        public function setValidade(string $novaValidade) : void{
+            $this->validadeCnh = $novaValidade;
+        }
+
+        public function getValidade() : string {
+            return $this->validadeCnh;
+        }
+    }
+
+?>
---
+++ Classes/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once 'Motorista.php';
+
+    $motorista = new Motorista('Gabriel','1111111111','2222222','12/02/2022');
+
+    echo $motorista->getNome();
+
+?>
